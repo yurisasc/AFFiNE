7fac2afafcd9da291c7a5fdb82689237d58b1c99
Yuris Cakranegara <yuris_aryansyah@yahoo.com>
2025-04-25 02:37:47 +1000

    feat(server): allow any models

--- packages/backend/server/src/plugins/copilot/providers/openai.ts
+++ packages/backend/server/src/plugins/copilot/providers/openai.ts
@@ -327,6 +327,16 @@ export class OpenAIProvider extends CopilotProvider<OpenAIConfig> {
     return result;
   }
 
+  /**
+   * Assume the models are always available.
+   * If there are any issues, check the admin panel to verify the models.
+   * @param _ model name
+   * @returns true
+   */
+  override isModelAvailable(_: string): Promise<boolean> | boolean {
+    return true;
+  }
+
   protected override setup() {
     super.setup();
     this.#instance =
